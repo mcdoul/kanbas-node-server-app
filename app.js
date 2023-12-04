094ac53ca2eefcb4039dc329f86d9244286ce3a8
Charlotte Yang <charlotteyang@Charlottes-MacBook-Air.local>
2023-12-03 21:44:16 -0800

    Assignment6 change frontend url

--- app.js
+++ app.js
@@ -12,27 +12,25 @@ import session from "express-session";
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
 // const CONNECTION_STRING = "mongodb+srv://kanbas:kanbas@kanbas.z9fawbv.mongodb.net/kanbas?retryWrites=true&w=majority"
 mongoose.connect(CONNECTION_STRING);
-// console.log("env" , process.env)
 
 
 const app = express();
 app.use(cors({
     credentials: true,
-    // origin: process.env.FRONTEND_URL
-    origin: "http://localhost:3000",}
+    origin: process.env.FRONTEND_URL,}
     ));
 const sessionOptions = {
     secret: "any string",
     resave: false,
     saveUninitialized: false,
     };
-// if (process.env.NODE_ENV !== "development") {
-//     sessionOptions.proxy = true;
-//     sessionOptions.cookie = {
-//         sameSite: "none",
-//         secure: true,
-//     };
-//     }
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+    }
       
 app.use(
 session(sessionOptions)
